d51a8e25517466b1996b74810860198cf5071ef4
Tomoaki Sugiayma <sti320a@gmail.com>
2019-05-20 22:10:57 +0900

    Update

--- napsack.cpp
+++ napsack.cpp
@@ -6,20 +6,28 @@ using namespace std;
 const int n=6, W=8;
 const int w[n] = {2,1,3,2,1,5}, v[n]={3,2,6,1,3,85};
 
+int dp[10][10];
+
 int rec(int i, int cap) {
+    if (dp[i][cap] != -1) {
+        return dp[i][cap];
+    }
+
+    int res;
     if (i == n) {
-        return 0;
+        res = 0;
     } else if (cap < w[i]) {
-        return rec(i + 1, cap);
+        res = rec(i + 1, cap);
     } else {
-        return max(
+        res = max(
             rec(i + 1, cap),
             rec(i + 1, cap - w[i]) + v[i]
         );
     }
+    return dp[i][cap] = res;
 }
 
 int main() {
-    // memset(dp, -1, sizeof(dp));
+    memset(dp, -1, sizeof(dp));
     cout << rec(0, W) << endl;
 }
